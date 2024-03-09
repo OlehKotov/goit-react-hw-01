c01843bbaa193909dccf9d4ac5ae107e2b33dae0
OlehKotov <mail.oleg456@gmail.com>
2024-03-09 12:23:04 +0200

    update

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,7 +3,7 @@ import clsx from "clsx";
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <div className={css.friendListItem}>
+    <div className={css.friendListItemImg}>
       <img src={avatar} alt="Avatar" width="48" />
       <p className={css.friendListItemName}>{name}</p>
       <p className={clsx(css.friendListItemStat, { [css.online]: isOnline })}>
